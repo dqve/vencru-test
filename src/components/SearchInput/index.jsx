038f9c6897783f28cdc7dacac52e0140472f2f68
David Ayo <davidayo@David-Ayo-MacBook-Pro.local>
2022-09-04 18:35:25 +0100

    <additional> Customizing the scrollbar

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -12,11 +12,11 @@ const SearchBar = ({ users }) => {
     <div className="relative inline-block text-left w-full">
       <div className='relative'>
         <span className=" absolute text-gray-500 sm:text-sm inline-flex h-full justify-center align-center m-[0.65rem]"><Search /></span>
-        <input onFocus={() => setState(!state)} onBlur={() => setState(!state)} className="inline-flex w-full justify-center rounded-lg border border-solid border-gray-300 bg-white px-4 py-2 pl-10 font-medium text-gray-700 focus:rounded-lg focus:shadow-purple-action focus:outline-none focus:ring-4 focus:ring-purple-30 focus:ring-offset-[0.5px] focus:ring-offset-purple-300" id="menu-button" ariaExpanded="true" ariaHaspopup="true" placeholder='Search' />
+        <input onFocus={() => setState(!state)} onBlur={() => setState(!state)} className="inline-flex w-full justify-center rounded-lg border border-solid border-gray-300 bg-white px-4 py-2 pl-10 font-medium text-gray-700 focus:rounded-lg focus:shadow-purple-action focus:outline-none focus:ring-4 focus:ring-purple-30 focus:ring-offset-[0.5px] focus:ring-offset-purple-300" id="menu-button" placeholder='Search' />
       </div>
 
 
-      <div className={` ${state ? "block" : "hidden"} absolute  mt-2 w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu" ariaOrientation="vertical" ariaLabelledby="menu-button" tabIndex="-1">
+      <div className={` ${state ? "block" : "hidden"} absolute  mt-2 w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`} role="menu"   tabIndex="-1">
         <div className="py-1" role="none">
           {users ? users.map((each, key) =>
             <a key={key} href="#" className="text-gray-700 block px-4 py-2 hover:bg-grey hover:py-2 hover:px-4text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">
--- src/components/SettingsNav/index.jsx
+++ src/components/SettingsNav/index.jsx
@@ -4,12 +4,11 @@ export default function SettingsNav() {
     return (
 
         <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-            <a
-                href="#"
+            <button
                 className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-grey-700 hover:bg-gray-50 focus:z-20"
             >
                 My details
-            </a>
+            </button>
             <a
                 href="#"
                 className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-grey-700 hover:bg-gray-50 focus:z-20"
